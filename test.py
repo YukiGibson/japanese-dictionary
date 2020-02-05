d62a43065382e1cf585609e0ae19849ccd0068f0
Laureano <lagasa95@hotmail.com>
2020-02-05 16:01:58 -0600

    added cards

--- test.py
+++ test.py
@@ -2,8 +2,14 @@ import api
 import pprint
 # getting file content
 
-new_request = api.ApiRequests("hello").get_content()["data"]
+new_request = api.ApiRequests("悪い").get_content()["data"]
 new_key = ""
-for item in new_request:
-    pprint.pprint(item["slug"])
+
+for value in range(2):
+    # Kanji
+    pprint.pprint(new_request[value]["japanese"][0]["word"])
+    # Furigana
+    pprint.pprint(new_request[value]["japanese"][0]["reading"])
+
+    pprint.pprint(new_request[value]["senses"][0]["english_definitions"])
 
